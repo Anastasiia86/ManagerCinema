646f5aeeca86f0c924bf20920d8e7bea1fda9450
Anastasiia Utina <anastasiiaprekrasnaiia@gmail.com>
2022-11-02 17:44:03 +0300

    modified files

--- src/main/java/ru/netology/cinema/Cinema.java
+++ src/main/java/ru/netology/cinema/Cinema.java
@@ -1,11 +1,8 @@
 package ru.netology.cinema;
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 @AllArgsConstructor
-@NoArgsConstructor
-@Data
+
+
 public class Cinema {
     private int id;
     private String name;
